19080b07d0f657e8b517a927ea26b26494bb24b3
Jorge Vargas <jvargas18@ucol.mx>
2021-05-14 03:19:56 -0500

    Add bootstrap

--- app.js
+++ app.js
@@ -18,17 +18,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  '/assets/vendor/bootstrap',
+  express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist'))
+);
+app.use(
+  '/assets/vendor/jquery',
+  express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
+app.use(
+  '/assets/vendor/popper.js',
+  express.static(
+    path.join(__dirname, 'node_modules', 'popper.js', 'dist', 'umd')
+  )
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
