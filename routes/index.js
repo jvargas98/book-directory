e76b966438e9dc06da08c0715920624b150c9d7e
Jorge Vargas <jvargas18@ucol.mx>
2021-05-16 14:33:53 -0500

    Add user auth views

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,16 @@ const booksController = require('../controllers/books_controller');
 /* GET home page. */
 router.get('/', booksController.index);
 
+router.get('/sign_up', (req, res, next) => {
+  res.render('signup', {
+    title: 'Sign up',
+  });
+});
+
+router.get('/sign_in', (req, res, next) => {
+  res.render('signin', {
+    title: 'Sign in',
+  });
+});
+
 module.exports = router;
