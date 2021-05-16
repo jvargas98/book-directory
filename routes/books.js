81f8c38da63b6e1beb84aad61ac5c33bacff4044
Jorge Vargas <jvargas18@ucol.mx>
2021-05-15 20:19:40 -0500

    Add books controller

---
+++ routes/books.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const booksController = require('../controllers/books_controller');
+
+router.get('/new', booksController.new);
+router.get('/view', booksController.show);
+router.get('/edit', booksController.edit);
+router.post('/destroy', booksController.destroy);
+router.post('/', booksController.create);
+
+module.exports = router;
