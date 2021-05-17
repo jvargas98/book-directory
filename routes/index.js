0f35e1af10b6299fbeb94f7a3b88bcf1b8a245cf
Jorge Vargas <jvargas18@ucol.mx>
2021-05-17 11:51:05 -0500

    Add user controller and new routes for user auth

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,55 @@
+/* eslint-disable consistent-return */
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 
+const usersController = require('../controllers/users_controller');
 const booksController = require('../controllers/books_controller');
-/* GET home page. */
-router.get('/', booksController.index);
 
-router.get('/sign_up', (req, res, next) => {
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/sign_in');
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  next();
+}
+
+/* GET home page. */
+router.get('/', checkAuthenticated, booksController.index);
+
+router.get('/sign_up', checkNotAuthenticated, (req, res, next) => {
   res.render('signup', {
     title: 'Sign up',
   });
 });
 
-router.get('/sign_in', (req, res, next) => {
+router.post('/sign_up', checkNotAuthenticated, usersController.create);
+
+router.get('/sign_in', checkNotAuthenticated, (req, res, next) => {
   res.render('signin', {
     title: 'Sign in',
+    message: req.flash('message'),
   });
+});
+
+router.post(
+  '/sign_in',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/sign_in',
+    failureFlash: true,
+  }),
+);
+
+router.get('/logout', (req, res) => {
+  req.logOut();
+  res.redirect('/sign_in');
 });
 
 module.exports = router;
